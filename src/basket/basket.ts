1a8c0820676541ae3cfd9b66d522d9539013c8f8
Richard <richard@adcalls.nl>
2022-08-08 11:00:40 +0200

    Update basket.ts
    Added the endpoints and removed declare

--- src/basket/basket.ts
+++ src/basket/basket.ts
@@ -2,16 +2,20 @@ import { JumboObject } from '../base/jumboObject';
 import { AdditionalRequestOptions } from '../jumbo';
 import { BasketModel } from './basketModel';
 
-export declare class Basket extends JumboObject {
+export class Basket extends JumboObject {
     /**
      * Gets basket 
      */
-    getMyBasket(additionalRequestOptions?: AdditionalRequestOptions): Promise<BasketModel>;
+    getMyBasket(additionalRequestOptions?: AdditionalRequestOptions): Promise<BasketModel> {
+        return await this.jumbo.get(`basket/`, additionalRequestOptions, true);
+    }
     /**
      * @param productJson consists of sku quantity 
      * @param additionalRequestOptions 
      */
-    addToBasket(productJson: any, additionalRequestOptions?: AdditionalRequestOptions): Promise<BasketModel>;
+    addToBasket(productJson: any, additionalRequestOptions?: AdditionalRequestOptions): Promise<BasketModel> {
+        return await this.jumbo.put(`basket/`, productJson, additionalRequestOptions, true);
+    }
 }
 
 export {};
